3ebb4f8e0eaa2bf1ff0dbd2ca89bb19608642aa2
ashutosh117 <ashutoshpandey.5891@gmail.com>
2018-07-13 11:50:44 +0530

    Update twitter_graph.py

--- twitter_graph.py
+++ twitter_graph.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 21 12:55:52 2018
 
-@author: t1
-live graphing twitter sentiment
-"""
+#live graphing twitter sentiment
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
